f0c3e10e3c8e07edc7bb0e4801446cc2f1f77e1e
minhquanlk <85548761+minhquanlk@users.noreply.github.com>
2021-11-20 19:15:29 +0700

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,29 @@
+#include "DataGenerator.h"
+using namespace std;
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void quickSort(int* arr, int left, int right, unsigned long long& countcmp) {
+      int i = left, j = right;
+      int pivot = arr[(left + right) / 2];
+      while (++countcmp && i <= j) {
+            while (++countcmp && arr[i] < pivot)
+                  i++;
+            while (++countcmp && arr[j] > pivot)
+                  j--;
+            if (++countcmp && i <= j) {
+                  swap(arr[i], arr[j]);
+                  i++;
+                  j--;
+            }
+      };
+
+      if (++countcmp && left < j)
+            quickSort(arr, left, j, countcmp);
+      if (++countcmp && i < right)
+            quickSort(arr, i, right, countcmp);
+}
