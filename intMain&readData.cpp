9978e5a3a490557445b7ccdb1202d7f6b0787bac
Quan Nguyen <85548761+minhquanlk@users.noreply.github.com>
2021-11-24 14:14:23 +0700

    Update intMain&readData.cpp

--- intMain&readData.cpp
+++ intMain&readData.cpp
@@ -1,35 +1,40 @@
-char* filePath(int dataCase, int dataSize)
+string filePath(int dataCase, int dataSize)
 {
-	char* path = new char[41];
-	switch (dataCase)
-		{
-		case 0:	// ngẫu nhiên
-			strcat(path, "D:/THCTDL/Lab3/DataRun/DataRandom_");
-			cout << "DataRandom - Size: ";
-			break;
-		case 1:	// có thứ tự
-			strcat(path, "D:/THCTDL/Lab3/DataRun/DataSorted_");
-			cout << "DataSorted - Size: ";
-			break;
-		case 2:	// có thứ tự ngược
-			strcat(path, "D:/THCTDL/Lab3/DataRun/DataReverse_");
-			cout << "DataReverse - Size: ";
-			break;
-		case 3:	// gần như có thứ tự
-			strcat(path, "D:/THCTDL/Lab3/DataRun/DataNearlySorted_");
-			cout << "DataNearlySorted - Size: ";
-			break;
-		default:
-			printf("Error: unknown data type!\n");
-		}
-	cout << dataSize;
-	char size[6];
-	itoa(dataSize, size, 10);
-	strcat(path, size);
-	strcat(path, ".csv");
-	return path;
-}  
-
+    char path[51] = "";
+    switch (dataCase)
+        {
+        case 0:	// ngẫu nhiên
+            strcat(path, "D:/THCTDL/Lab3/DataRun/DataRandom_");
+            cout << "DataRandom - Size: ";
+            break;
+        case 1:	// có thứ tự
+            strcat(path, "D:/THCTDL/Lab3/DataRun/DataSorted_");
+            cout << "DataSorted - Size: ";
+            break;
+        case 2:	// có thứ tự ngược
+            strcat(path, "D:/THCTDL/Lab3/DataRun/DataReverse_");
+            cout << "DataReverse - Size: ";
+            break;
+        case 3:	// gần như có thứ tự
+            strcat(path, "D:/THCTDL/Lab3/DataRun/DataNearlySorted_");
+            cout << "DataNearlySorted - Size: ";
+            break;
+        default:
+            printf("Error: unknown data type!\n");
+        }
+    cout << dataSize << endl;
+    char size[7];
+    itoa(dataSize, size, 10);
+    strcat(path, size);
+    strcat(path, ".csv");
+    return path;
+}
+void printArray(int* arr, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
 int main()
 {
 	ifstream inputData;
@@ -39,23 +44,22 @@ int main()
 	for (int i = 0; i < 4; i ++)
 		for (int j = 0; j < 6; j++)
 		{
-			countcmp = 0;
-			char* filepath = filePath(i, n[j]);
+			string filepath = filePath(i, n[j]);
 			inputData.open(filepath, ios::in);
 			int* arr = new int[n[j]];
 			for (int k = 0; k < n[j]; k++)
 			{
-				inputData >> arr[k]; 
+				inputData >> arr[k];
 			}
+			//printArray(arr, n[j]);
+			countcmp = 0;
 			start = clock();
-			quickSort(arr, 0, n[j] - 1, countcmp);  //sort algorithm
+			flashSort(arr, n[j], countcmp);
 			end = clock();
 			double runtime = ((double)(end - start));
 			cout << endl << countcmp << " " << runtime << endl;
+			inputData.close();
 		}
 	//printArray(arr, n);
-	
-
-	inputData.close();
 	return 0;
 }
