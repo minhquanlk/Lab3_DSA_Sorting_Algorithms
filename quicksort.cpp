a63f8bcddc7bbdd01e7901c9a871dcf4e3734962
Quan Nguyen <85548761+minhquanlk@users.noreply.github.com>
2021-11-24 14:34:30 +0700

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,12 +1,4 @@
-#include "DataGenerator.h"
-
-void swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-void quickSort(int* arr, int left, int right, unsigned long long& countcmp) {
+quickSort(int* arr, int left, int right, unsigned long long& countcmp) {
       int i = left, j = right;
       int pivot = arr[(left + right) / 2];
       while (++countcmp && i <= j) {
